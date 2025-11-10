643cea5755cd9c43e3340813ba2a4d3ee72a60ad
Renan Raposo <134230060+RaposoRenan@users.noreply.github.com>
2025-11-10 14:00:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,12 @@ function Home() {
   return <h1>Caroline RAPOSO, você é a melhor mãe do mundo!!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
+
 export default Home;
